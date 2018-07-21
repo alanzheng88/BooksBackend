39946b186fd5752c8c75d7a1a86255369ef02641
Alan Zheng <alanz@sfu.ca>
2018-07-21 14:16:23 -0700

    add back missing CORS

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from .config import config_by_name
 from ..__init__ import api
 #from pdb import set_trace
@@ -9,8 +10,9 @@ db = SQLAlchemy()
 # config_name is an object with the following keys: dev, test, prod
 def create_app(config_name):
   app = Flask(__name__)
+  CORS(app)
   # merge my custom configs into the default app.config
   app.config.from_object(config_by_name[config_name])
   api.init_app(app)
-  db.init_app(app)  
+  db.init_app(app)
   return app
